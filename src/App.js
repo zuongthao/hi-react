5481ab663431eee8963914fd87a0a4b895746003
zuongthao <zuongthao@gmail.com>
2019-09-09 21:28:14 +0700

    Graphql for blog post

--- src/App.js
+++ src/App.js
@@ -1,45 +1,39 @@
-import {execute, makePromise} from 'apollo-link';
-import {HttpLink} from 'apollo-link-http';
+import { ApolloClient } from 'apollo-client';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { HttpLink } from 'apollo-link-http';
 
 import gql from "graphql-tag";
 
-const uri = 'https://countries.trevorblades.com/';
-const link = new HttpLink({uri});
+const uri = 'http://gclock.local/index.php/graphql/';
+const cache = new InMemoryCache();
+const link = new HttpLink({
+    uri: uri
+})
 
-const QUERY = gql`
-    query {
-        countries {
-            name
-            code
-        }
-    }
-`;
-
-const operation = {
-    query: QUERY,
-    variables: {}, //optional
-    operationName: null, //optional
-    context: {
-        headers: {
-            authorization: "asdasdasdasdasd"
-        }
-    }, //optional
-    extensions: {} //optional
-};
-
+const client = new ApolloClient({
+    cache,
+    link
+})
 
 function App() {
-    // execute returns an Observable so it can be subscribed to
-    execute(link, operation).subscribe({
-        next: data => console.log(`received data: ${JSON.stringify(data, null, 2)}`),
-        error: error => console.log(`received error ${error}`),
-        complete: () => console.log(`complete`),
-    });
-
-    // For single execution operations, a Promise can be used
-    makePromise(execute(link, operation))
-        .then(data => console.log(`received data ${JSON.stringify(data, null, 2)}`))
-        .catch(error => console.log(`received error ${error}`));
+    client
+    .query({
+        query: gql`
+            {
+                cmsPage(id: 1) {
+                    url_key
+                    title
+                    content
+                    content_heading
+                    page_layout
+                    meta_title
+                    meta_description
+                    meta_keywords
+                }
+            }
+        `
+    })
+    .then(result => console.log(result));
     return null;
 }
 
